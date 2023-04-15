16925d17fff60048035d632f65675c5355912d77
mortalpjero <mortalpjero@gmail.com>
2023-04-15 12:27:23 -0700

    feat: updated and optimized gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,42 @@
-'use strict';
-
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sync = require('browser-sync').create();
 
-function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
+const styles = () => {
+  return gulp.src("src/scss/style.scss")
+  .pipe(plumber())
+  .pipe(sourcemap.init())
+  .pipe(sass())
+  .pipe(postcss([autoprefixer()]))
+  .pipe(sourcemap.write('.'))
+  .pipe(gulp.dest('src/css'))
+  .pipe(sync.stream());
+}
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
+exports.styles = styles;
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'src'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+exports.server = server;
+
+const watcher = () => {
+  gulp.watch('src/scss/**/*.scss', gulp.series('styles'));
+  gulp.watch('src/*.html').on('change', sync.reload);
+}
+
+exports.start = gulp.series(
+  styles, server, watcher
+)
